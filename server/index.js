824b8a963e9ed4e020949e483c646d4dced5edf8
Hrabenka7 <markettka@email.cz>
2022-11-18 23:40:30 +0100

    add random image api call

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const API = "https://dog.ceo/api/breeds"
+
+app.get("/random", (req, res, next) => { 
+  axios.get(API + "/image/random")
+   //parse response and add breed to the return
+  .then(response => res.json(response.data))
+  .catch(err => next(err));
+});
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
